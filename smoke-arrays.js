a7a1b8bc11de3f8e77e49c6416644a0f1b3d4c01
smanghise <35621921+smanghise@users.noreply.github.com>
2018-02-17 12:03:50 -0800

    Update smoke-arrays.js

--- smoke-arrays.js
+++ smoke-arrays.js
@@ -1,1 +1,40 @@
+var x = [];
+var y = [];
+var r = 0;
 
+function setup() {
+  createCanvas(400, 400);
+	
+	for(var i = 0; i < 100; i = i +1) {
+		x[i] = random(200,210)
+		y[i] = random(270,290)
+	}
+}
+  
+function draw() {
+  background(0);
+  noStroke();
+
+  // draw smokestack
+  fill(255);
+  rect(195, height, 30, -100);
+
+	for(var i = 0; i < 100; i = i +1) {
+  	push();
+  	fill(y[i]);
+  	translate(x[i], y[i]);
+  	rotate(r);
+ 	  rect(-10, -10, 20, 20);
+  	pop();
+  
+  	y[i] -= 1;
+  	
+  	r += 0.05
+  
+ 	 if (y[i] < -150) {
+  	y[i] = 290;
+		 
+	 }
+
+  }
+}
