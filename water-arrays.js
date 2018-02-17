88aa9a7ab3a33879a5e93e46dbbcf7233c1adf7b
smanghise <35621921+smanghise@users.noreply.github.com>
2018-02-17 11:36:02 -0800

    Update water-arrays.js

--- water-arrays.js
+++ water-arrays.js
@@ -10,8 +10,8 @@ function setup() {
   for (var i = 0; i < 100; i = i + 1) {
     x[i] = width/2;
     y[i] = height/2;
-		xSpeed[i] = random(-2,2)
-		ySpeed[i] = random(1,3)
+    xSpeed[i] = random(-2,2)
+    ySpeed[i] = random(1,3)
   }
 }
 
@@ -19,10 +19,10 @@ function draw() {
   background(0);
   noStroke();
 	
-	  // draw pipe
+  // draw pipe
   rect(0, 200, 230, 20);
 
   for (var i = 0; i < 100; i = i + 1) {
     ellipse(x[i],y[i], 10);
-		x[i] = x[i] + xSpeed[i];
+    x[i] = x[i] + xSpeed[i];
     y[i] = y[i] + ySpeed[i];
