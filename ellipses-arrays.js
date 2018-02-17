b0176308e30213f8031b9b488dcd3a560045dbea
smanghise <35621921+smanghise@users.noreply.github.com>
2018-02-17 09:40:10 -0800

    Update ellipses-arrays.js

--- ellipses-arrays.js
+++ ellipses-arrays.js
@@ -1,6 +1,7 @@
 var x = [];
 var y = [];
 var d = [];
+var colors = [];
 
 function setup() {
   createCanvas(400, 400);
@@ -9,7 +10,9 @@ function setup() {
   for (var i = 0; i < 100; i = i + 1) {
     x[i] = random(0, width);
     y[i] = random(0, height);
-		d[i] = random(10,60)
+    d[i] = random(10,60);
+    colors[i] = color(random(255), random(255), random(255))
+		
   }
 }
 
@@ -18,6 +21,8 @@ function draw() {
   noStroke();
 
   for (var i = 0; i < 50; i = i + 1) {
+    fill(colors[i]);
     ellipse(x[i], y[i], d[i]);
+		
   }
 }
